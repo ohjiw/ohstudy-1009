2da07734fe6fb2eaf05a01e71761c8f9cd3f5009
ohjiw <115399976+ohjiw@users.noreply.github.com>
2022-10-09 19:50:32 +0900

    second file

--- python01.py
+++ python01.py
@@ -1,16 +1,19 @@
 a=10
 b=20
-print(a+b)
+print(a*b)
 
 c=30
 d=40
-print(d-c)
+print(d/c)
 print(a+b+c+d)
 
 for i in range(a):
   print(i*b)
   print(i*c)
   print(i*d)
+  if i%2==0:
+    print("짝수")
+  
 
   
   
